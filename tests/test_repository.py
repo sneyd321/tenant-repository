6847d10fe968cfae39ca5c8f2a06c00887d8fefa
Ryan Sneyd <rts1234567@hotmail.com>
2022-10-10 11:26:27 -0400

    removed async markers in pytest

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -5,7 +5,14 @@ from models.models import Tenant
 import pytest, asyncio, json
 
 
-    
+@pytest.fixture(scope="session")
+def event_loop():
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
+
+@pytest.mark.asyncio
 async def test_Tenant_Service_returns_an_error_message_conflict_in_database_when_an_integrity_error_occurs():
     db = DB("test", "homeowner", "localhost", "roomr")
     repository = Repository(db)
@@ -16,7 +23,7 @@ async def test_Tenant_Service_returns_an_error_message_conflict_in_database_when
     monad = await repository.insert(tenant)
     assert monad.error_status == {"status": 409, "reason": "Failed to insert data into database"}
    
-
+@pytest.mark.asyncio
 async def test_Tenant_Service_returns_an_empty_list_when_retrieving_a_list_of_zero_items():
     db = DB("test", "homeowner", "localhost", "roomr")
     repository = Repository(db)
@@ -39,6 +46,7 @@ async def test_Tenant_Service_returns_an_error_message_when_account_does_not_exi
  
     assert monad.error_status == {"status": 404, "reason": "No data"}
 
+@pytest.mark.asyncio
 async def test_Tenant_Service_returns_an_error_message_when_password_is_invalid():
     db = DB("test", "homeowner", "localhost", "roomr")
     repository = Repository(db)
